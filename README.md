c00c9f45a20dc5cc54cdce0e8db6b1650ee73ae3
Alexander Konstantinov <kottinov@gmail.com>
2025-12-27 16:01:20 +0200

    the_project scaffold

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ### Chapter 1
 
 - [1.1.](https://github.com/kottinov/tkt-21027/tree/1.1/log_output)
+- [1.2.](https://github.com/kottinov/tkt-21027/tree/1.2/the_project)
---
+++ the_project/src/main.rs
@@ -0,0 +1,28 @@
+use std::net::TcpListener;
+
+use tracing_subscriber::fmt::time::UtcTime;
+use tracing_subscriber::EnvFilter;
+
+use the_project::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    tracing_subscriber::fmt()
+        .with_timer(UtcTime::rfc_3339())
+        .with_env_filter(filter)
+        .init();
+
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse::<u16>()
+        .expect("PORT must be a valid port number");
+
+    let address = format!("0.0.0.0:{}", port);
+    let listener = TcpListener::bind(&address)?;
+
+    tracing::info!("Server started in port {}", port);
+
+    run(listener)?.await
+}
