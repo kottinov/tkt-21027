af696cf47091e9c60b1f8fe41ba0a1fecbbfeddc
Alexander Konstantinov <kottinov@gmail.com>
2025-12-27 16:53:24 +0200

    add service manifest and bump health check response

--- the_project/src/lib.rs
+++ the_project/src/lib.rs
@@ -4,13 +4,16 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok()
+        .content_type("application/json; charset=utf-8")
+        .body(r#"{"status":"ok"}"#)
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .wrap(tracing_actix_web::TracingLogger::default())
+            .route("/", web::get().to(health_check))
             .route("/healthz", web::get().to(health_check))
     })
     .listen(listener)?
